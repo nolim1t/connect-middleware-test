50acb39ae5a14f5ba26f269c454bb84bd6b04ab5
Barry (@nolim1t) <nolimit@perceptionz.net>
2011-12-01 15:00:29 +1100

    Add shell execute to the web server to demonstrate subprocesses

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 var sys = require('sys');
 var uuid = require('node-uuid');
 var connect = require('connect');
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+
 var ws = connect.createServer(
     connect.logger(),
     connect.bodyParser(),
@@ -18,5 +22,15 @@ var ws = connect.createServer(
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify({guid: uuid()}));
         });
+        app.get('/pwd', function(req, res, next) {
+            res.setHeader('Content-Type', 'text/plain');
+            child = exec('pwd', function (err,stderr,stdout) {
+                if (err) {
+                    res.end('An error ' + err + ' has occured');
+                } else {
+                    res.end('stderr="' + stderr + '";stdout="' + stdout + '"');                
+                }                
+            })
+        })
     })
 ).listen(4000);
